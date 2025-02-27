1646bb9b928aed39b44752df161986cb75413256
khany <khany@NAKEKELOCARES>
2025-02-27 21:14:57 +0200

    Add project files.

---
+++ S1L03/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace S1L03.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+
+        public ActionResult ListPeople()
+        {
+            List<Models.PersonModel> people = new List<Models.PersonModel>();
+
+            people.Add(new Models.PersonModel { FirstName = "Name 1", LastName = "Surname 1", Age = 1, IsAlive = true });
+            people.Add(new Models.PersonModel { FirstName = "Name 2", LastName = "Surname 2", Age = 2, IsAlive = false });
+            people.Add(new Models.PersonModel { FirstName = "Name 3", LastName = "Surname 3", Age = 3, IsAlive = true });
+            people.Add(new Models.PersonModel { FirstName = "Name 4", LastName = "Surname 4", Age = 4, IsAlive = false });
+            people.Add(new Models.PersonModel { FirstName = "Name 5", LastName = "Surname 5", Age = 5, IsAlive = true });
+
+            return View(people);
+
+        }
+ 
+    }
+}
---
+++ S1L03/Models/PersonModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations; // Add to project
+//Ensure that CodeLens is activated
+//Select >> Tools >> Options >> Text Editor >> All Languages >> CodeLens
+namespace S1L03.Models
+{
+    public class PersonModel
+    {
+        //Quick property - type "prop" press tab x2
+        //Complete the property details
+        //This is the "Model" of "data source"
+
+        [Display(Name = "First Name")] //Add as decorator
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")] //Add as decorator
+        public string LastName { get; set; }
+        
+        [Display(Name = "Current Age")] //Add as decorator
+        public int Age { get; set; } = 0;
+
+        [Display(Name = "Living Status")] //Add as decorator
+        // [Required(ErrorMessage = "Name is mandatory")]=> Example of manditory decorator
+        public bool IsAlive { get; set; } = true;
+    }
+}
